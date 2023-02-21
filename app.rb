e17e705922968aee3736dff7c7a3e65255395bed
hannaostlind <93259678+hannaostlind@users.noreply.github.com>
2023-02-21 11:20:27 +0100

    update
    fixat inlogg, funkar dock inte. lagt till länkar och users i databasen

--- app.rb
+++ app.rb
@@ -7,7 +7,32 @@ enable :sessions
 
 output = []
 
-get('/start') do
-
+get('/') do
     slim(:start)
+end
+
+get('/submit') do
+    slim(:submit)
+end
+
+get('/overview') do
+    slim(:overview)
+end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+
+    if (password == password_confirm)
+        #lägg till användare
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite::Database.new{'db/dogs.db'}
+        db.execute("INSERT INTO users (username,pwdigest) VALUES {?,?}", username,password_digest)
+        redirect('/')
+
+    else
+        #felhantering
+        "Lösenorden matchade inte!"
+    end
 end
